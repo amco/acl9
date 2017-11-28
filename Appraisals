23ad8c890580d004bc191d133028ccc2c3c1b2f8
Eddy Roberto <eddyr.morales@gmail.com>
2017-11-28 14:06:05 -0600

    Change Appraisals config to just ruby 5

--- Appraisals
+++ Appraisals
@@ -2,3 +2,8 @@ appraise "rails-5.0" do
   gem 'rails', '5.0.0.beta.1'
 end
 
+appraise "rails-5.1" do
+  gem 'rails', '5.1.0'
+end
+
+
--- test/dummy/app/models/uuid.rb
+++ test/dummy/app/models/uuid.rb
@@ -1,4 +1,4 @@
 class Uuid < ActiveRecord::Base
-  self.primary_key = "uuid"  
+  self.primary_key = 'uuid'
   acts_as_authorization_object
 end
