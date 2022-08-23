c7d3bae7601618c79ea568456c6dcd09e663b1ce
night91 <eddyr.morales@gmail.com>
2022-08-23 10:45:47 -0500

    Remove support for rails 5.X

--- test/dummy/config/initializers/assets.rb
+++
@@ -1,5 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
